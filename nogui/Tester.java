f7eb3b2e8053fc30b2953989175b72c35397e162
Luke Woollett <lwoollett@yahoo.co.nz>
2017-04-11 13:31:02 +1200

    Wep

--- nogui/Tester.java
+++ nogui/Tester.java
@@ -2,9 +2,12 @@ package nogui;
 
 public class Tester {
 	private void testplayers(){
-		//xd
+		Player tp1 = new Player("Test Player 1");
+		System.out.println(tp1.getName());
+		System.out.println(tp1.getMoney());
 	}
 	public static void main(String[] args){
-		//xd
+		Tester t = new Tester();
+		t.testplayers();
 	}
 }
